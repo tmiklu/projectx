eabc95d572aad3552a12639978793d257ae2a93e
tmiklu <61796331+tmiklu@users.noreply.github.com>
2026-04-28 17:57:30 +0200

    feat(main.py): Add SSO login feature to main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
     print("second feature.")
     print("login feature.")
     print("contact form, fix typo in form")
+    print("add sso login for system.")
 
 
 if __name__ == "__main__":
