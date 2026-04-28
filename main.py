38ecccb2535558bc38e4013cbdb9533f6508acb8
tmiklu <tomas.miklusicak@cloudops.sk>
2026-04-28 14:11:10 +0200

    feat(main.py): add first feature

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 def main():
     print("Hello from abc!")
-    print("First Feature!")
-    print("Second Feature!")
-    print("Third Feature!")
-    print("Fourth Feature!")
 
 
 if __name__ == "__main__":
