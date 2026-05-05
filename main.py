465abe1f0e60671a0aeb8c0a0942bf3101d6bd35
tmiklu <61796331+tmiklu@users.noreply.github.com>
2026-05-05 09:54:27 +0200

    fix: message for breaking change in main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
     print("new login form, extended!")
     print("add spam protection. FIX!")
     print("this is breaking change..., fix!")
-    print("another breaking change, extend this feature!!!")
+    print("another breaking change, extend this feature!!! Fix here.")
 
 
 if __name__ == "__main__":
