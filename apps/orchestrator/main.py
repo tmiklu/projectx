5534b1d3dbc70d309bf628fc09d13a2e5285d28c
tmiklu <61796331+tmiklu@users.noreply.github.com>
2026-05-18 09:00:05 +0200

    perf: Modify print statement for extended feature
    Updated print statement to include 'Perf' in the message.

--- apps/orchestrator/main.py
+++ apps/orchestrator/main.py
@@ -13,7 +13,7 @@ def main():
 
     print("New feature 1.6.0, Fix here! Fix.")
 
-    print("Extend new feature, fix. FIX!")
+    print("Extend new feature, fix. FIX! Perf")
 
     print("breaking change")
 
