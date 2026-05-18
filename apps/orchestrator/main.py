61275e1f9461d5fd52297bdee478c923dec94a43
tmiklu <61796331+tmiklu@users.noreply.github.com>
2026-05-18 10:08:29 +0200

    fix: print statement for new feature extension

--- apps/orchestrator/main.py
+++ apps/orchestrator/main.py
@@ -13,7 +13,7 @@ def main():
 
     print("New feature 1.6.0, Fix here! Fix. Perf. Perf.")
 
-    print("Extend new feature, fix. FIX! Perf, Feat")
+    print("Extend new feature, fix. FIX! Perf, Feat, fix")
 
     print("breaking change")
 
